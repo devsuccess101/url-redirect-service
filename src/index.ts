91a4755d7f27e9de1bee9a27073561ca93820a3a
Nguyen Huu Kim <13513658+kimyvgy@users.noreply.github.com>
2023-09-01 13:28:11 +0700

    feat: Init URL Redirect service

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+export default {
+  async fetch(request) {
+    const redirectMap = new Map([
+      ['/youtube', 'https://www.youtube.com/@devsuccess101'],
+      ['/subscribe', 'https://www.youtube.com/@devsuccess101?sub_confirmation=1'],
+      ['/github', 'https://github.com/devsuccess101'],
+      ['/viblo', 'https://viblo.asia/o/devsuccess101'],
+      ['/facebook', 'https://www.facebook.com/devsuccess101'],
+      ['/slack', 'https://devsuccess101.slack.com'],
+      ['/tiktok', 'https://www.tiktok.com/@devsuccess101'],
+    ]);
+
+    const requestURL = new URL(request.url);
+    const path = requestURL.pathname;
+    const location = redirectMap.get(path);
+
+    if (location) {
+      return Response.redirect(location, 301);
+    }
+
+    // If request not in map, return the original request
+    return fetch(request);
+  },
+};
